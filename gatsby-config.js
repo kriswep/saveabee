8ad657828c73d5ff23564ca495f3fe3b4b1d43f6
kriswep <kriswep@wetainment.com>
2019-08-16 21:27:39 +0200

    add netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,13 @@ module.exports = {
     },
   },
   plugins: [
+    `gatsby-plugin-netlify-cms`,
+    // {
+    //   resolve: 'gatsby-plugin-netlify-cms',
+    //   options: {
+    //     // modulePath: `${__dirname}/src/cms/cms.js`,
+    //   },
+    // },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
